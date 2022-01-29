6965f64d23dd2a07fb8134a411a45c085c30adcc
Xingyu-Liao <60574102+Xingyu-Liao@users.noreply.github.com>
2022-01-29 09:21:26 +0300

    Update Makefile.sh

--- Makefile.sh
+++ Makefile.sh
@@ -46,8 +46,7 @@ cp  ./tools/scripts/karect       ./programs/scripts/
 cp  ./tools/scripts/bwa       ./programs/scripts/
 
 #Unzip-all
-rm -rf ./tools
-unzip  unzip -o -d ./ ./tools.zip
+unzip  unzip -o -d ./tools ./tools.zip
 
 #Unzip-bowtie2
 rm -rf ./programs/bowtie2-2.4.4
